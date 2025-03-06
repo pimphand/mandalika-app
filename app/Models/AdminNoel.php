45e182be1929c3dd581153b62372de59ea4789c9
pimphand <faisaldwiki69@gmail.com>
2025-03-06 13:47:32 +0700

    add api

---
+++ app/Models/AdminNoel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdminNoel extends Model
+{
+    protected $table = 'admins_noel';
+
+    protected $guarded = [];
+}
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -423,7 +423,32 @@ Kemasan: ${packaging}`;
             });
         </script>
     @endauth
-
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-embedded-agent.js'></script>
+    <script>
+        window.addEventListener("DOMContentLoaded", function() {
+            window.AgentInitializer.init({
+                agentRenderURL: "https://agent.jotform.com/01956a1913957cf5b064b5234426a6002342",
+                rootId: "JotformAgent-01956a1913957cf5b064b5234426a6002342",
+                formID: "01956a1913957cf5b064b5234426a6002342",
+                queryParams: ["skipWelcome=1", "maximizable=1"],
+                domain: "https://www.jotform.com",
+                isDraggable: false,
+                background: "linear-gradient(180deg, #035C5F 0%, #035C5F 100%)",
+                buttonBackgroundColor: "#00433A",
+                buttonIconColor: "#FFFFFF",
+                variant: false,
+                customizations: {
+                    "greeting": "Yes",
+                    "greetingMessage": "Hai! Tanyakan Disini?",
+                    "openByDefault": "No",
+                    "pulse": "Yes",
+                    "position": "right",
+                    "autoOpenChatIn": "0"
+                },
+                isVoice: undefined
+            });
+        });
+    </script>
 </body>
 
 </html>
